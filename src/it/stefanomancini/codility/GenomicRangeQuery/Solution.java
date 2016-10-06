bb90f915cae843e09d4911c47eb319d0e85f59be
Stefano Mancini <stefano.mancini@gmail.com>
2016-10-07 00:03:56 +0200

    Lesson #5: second solution to GenomicRangeQuery - minor syntax improvements

--- src/it/stefanomancini/codility/GenomicRangeQuery/Solution.java
+++ src/it/stefanomancini/codility/GenomicRangeQuery/Solution.java
@@ -30,35 +30,19 @@ class Solution {
         int[] G = new int[N + 1];
         int[] T = new int[N + 1];
         for (int i = 1; i < N + 1; i++) {
-            A[i] = A[i - 1];
-            C[i] = C[i - 1];
-            G[i] = G[i - 1];
-            T[i] = T[i - 1];
             char nucleotide = S.charAt(i - 1);
-            switch (nucleotide) {
-                case 'A':
-                    A[i]++;
-                    break;
-                case 'C':
-                    C[i]++;
-                    break;
-                case 'G':
-                    G[i]++;
-                    break;
-                case 'T':
-                    T[i]++;
-                    break;
-            }
+            A[i] = A[i - 1] + ((nucleotide == 'A') ? 1 : 0);
+            C[i] = C[i - 1] + ((nucleotide == 'C') ? 1 : 0);
+            G[i] = G[i - 1] + ((nucleotide == 'G') ? 1 : 0);
+            T[i] = T[i - 1] + ((nucleotide == 'T') ? 1 : 0);
         }
         int M = P.length;
         int[] R = new int[M];
         for (int i = 0; i < M; i++) {
-            int left = P[i];
-            int right = Q[i];
-            int countA = A[right + 1] - A[left];
-            int countC = C[right + 1] - C[left];
-            int countG = G[right + 1] - G[left];
-            int countT = T[right + 1] - T[left];
+            int countA = A[Q[i] + 1] - A[P[i]];
+            int countC = C[Q[i] + 1] - C[P[i]];
+            int countG = G[Q[i] + 1] - G[P[i]];
+            int countT = T[Q[i] + 1] - T[P[i]];
             if (countA > 0)
                 R[i] = 1;
             else if (countC > 0)
